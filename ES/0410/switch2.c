3d58317fe0271ea908714eeb200d54ff071b7035
Chansik Yun <chansigi@gmail.com>
2019-04-10 11:45:22 +0900

    .

---
+++ ES/0410/switch2.c
@@ -0,0 +1,60 @@
+
+#if 0
+| Mode | Name    | wPi | BCM |
++------+---------+-----+-----+
+| IN   | GPIO. 4 | 4   | 23  |
+| IN   | GPIO. 5 | 5   | 24  |
++------+---------+-----+-----+
+#endif
+
+#include <wiringPi.h>
+#include <stdio.h>
+
+#define SW1 4
+#define SW2 5
+
+#define LED  27
+
+int main() {
+	wiringPiSetup();
+
+	pinMode(SW1, INPUT);
+	pinMode(SW2, INPUT);
+
+	pinMode(LED, OUTPUT);
+
+	while (1) {
+		if (digitalRead(SW1) == LOW) {  // 눌렀을 때
+			digitalWrite(LED, HIGH);
+			delay(500);
+			digitalWrite(LED, LOW);
+			delay(500);
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
