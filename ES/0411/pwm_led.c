ce1b5d56b8b4a5cdae28346aaac59b51e97897a8
Chansik Yun <chansigi@gmail.com>
2019-04-11 09:46:53 +0900

    .

--- ES/0411/pwm_led.c
+++ ES/0411/pwm_led.c
@@ -4,9 +4,9 @@
 
 #include <softPwm.h> // !!
 
-#define RGB_LED1 23    // HW PWM
-#define RGB_LED2 26    // HW PWM
-#define RGB_LED3 22    // HW PWM X 
+#define RGB_LED1 23    // HW PWM - RED
+#define RGB_LED2 26    // HW PWM - GREEN
+#define RGB_LED3 22    // HW PWM X - BLUE
 					   // OUTPUT 모드에서 직접 비율을 조절해야 한다.
 					   // -> SOFT PWM
 
@@ -32,24 +32,29 @@ int main(void) {
 	pwmWrite(RGB_LED2, 0);
 	softPwmWrite(RGB_LED3, 0);
 
+#if 1
 	while (1) {
 		for (bright = 0; bright < 1024; ++bright) {
 			softPwmWrite(RGB_LED3, bright);
-
-			// pwmWrite(RGB_LED2, bright);
-			// pwmWrite(RGB_LED2, bright);
+			delay(1);
+			pwmWrite(RGB_LED2, bright);
+			delay(1);
+			pwmWrite(RGB_LED2, bright);
 			delay(1);
 		}
 
 		for (bright = 1023; bright >= 0; --bright) {
 			softPwmWrite(RGB_LED3, bright);
-			// pwmWrite(RGB_LED2, bright);
-			// pwmWrite(RGB_LED2, bright);
+			delay(1);
+			pwmWrite(RGB_LED2, bright);
+			delay(1);
+			pwmWrite(RGB_LED2, bright);
 			delay(1);
 		}
 	}
 
 	return 0;
+#endif
 }
 
 
---
+++ ES/0411/pwm_servo.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <wiringPi.h>
+#include <softServo.h>  // !!!!
+
+#define SERVO_GPIO 25
+
+int main(void) {
+	if (wiringPiSetup() == -1) {
+		exit(1);
+	}
+
+	softServoSetup(SERVO_GPIO, -1, -1, -1, -1, -1, -1, -1);
+	// -250 ~ 1250
+	while (1) {
+		printf("0\n");
+		softServoWrite(SERVO_GPIO, 0);
+		getchar();
+
+		printf("-250\n");
+		softServoWrite(SERVO_GPIO, -250);
+		getchar();
+
+		printf("500\n");
+		softServoWrite(SERVO_GPIO, 500);
+		getchar();
+
+		printf("1250\n");
+		softServoWrite(SERVO_GPIO, 1250);
+		getchar();
+	}
+}
+
+
+
