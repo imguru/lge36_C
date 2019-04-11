13d0a40c1b141a5ddf00bc921c5b9f28fed47a8a
Chansik Yun <chansigi@gmail.com>
2019-04-11 09:28:22 +0900

    .

--- ES/0411/pwm_led.c
+++ ES/0411/pwm_led.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include <wiringPi.h>
 
-#define RGB_LED1 23
-#define RGB_LED2 26
+#include <softPwm.h> // !!
+
+#define RGB_LED1 23    // HW PWM
+#define RGB_LED2 26    // HW PWM
+#define RGB_LED3 22    // HW PWM X 
+					   // OUTPUT 모드에서 직접 비율을 조절해야 한다.
+					   // -> SOFT PWM
 
 int main(void) {
 	int bright;
@@ -18,18 +23,27 @@ int main(void) {
 	pinMode(RGB_LED1, PWM_OUTPUT);
 	pinMode(RGB_LED2, PWM_OUTPUT);
 
+	//-----
+	pinMode(RGB_LED3, OUTPUT);  
+	softPwmCreate(RGB_LED3, 0, 1024);
+	//-----
+
 	pwmWrite(RGB_LED1, 0);
 	pwmWrite(RGB_LED2, 0);
+	softPwmWrite(RGB_LED3, 0);
 
 	while (1) {
 		for (bright = 0; bright < 1024; ++bright) {
-			pwmWrite(RGB_LED2, bright);
+			softPwmWrite(RGB_LED3, bright);
+
+			// pwmWrite(RGB_LED2, bright);
 			// pwmWrite(RGB_LED2, bright);
 			delay(1);
 		}
 
 		for (bright = 1023; bright >= 0; --bright) {
-			pwmWrite(RGB_LED2, bright);
+			softPwmWrite(RGB_LED3, bright);
+			// pwmWrite(RGB_LED2, bright);
 			// pwmWrite(RGB_LED2, bright);
 			delay(1);
 		}
